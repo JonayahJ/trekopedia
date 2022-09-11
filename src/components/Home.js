989676cc7d0faeb9ab49e785a47d32a48c1d761f
Jonayah Jackson <jonayah@thinkhalcyon.com>
2022-09-11 09:52:15 -0400

    added starter styling for auth routes - DMaliva

---
+++ src/components/Home.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+
+const Home = () => {
+
+
+    return (
+        <>
+            <div className="p-4 box mt-3 text-center">
+                Welcome to Trekopedia
+            </div>
+            <div className="d-grid gap-2">
+                <Button variant="primary">
+                    Log out
+                </Button>
+            </div>
+        </>
+    );
+};
+
+export default Home;
